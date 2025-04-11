46eaaae1ecf4e09dbd2e461e22c1eac4a25425b9
Girdhar <greatgmang@gmail.com>
2025-04-11 18:18:07 +0530

    added chat functionality

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,6 @@
 import { WebSocket, WebSocketServer } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken"
 import {JWT_SECRET} from 'backend-common/config'
-import { SrvRecord } from "dns";
 
 const wss = new WebSocketServer({port:8080})
 
@@ -16,14 +15,20 @@ const users:User[] = []
 
 
 function checkUser(token:string):string|null{
-    const decoded = jwt.verify(token,JWT_SECRET as string)
-    if(typeof decoded === 'string')
-        return null
+    try{
+        const decoded = jwt.verify(token,JWT_SECRET as string)
+        if(typeof decoded === 'string')
+            return null
 
-    if(!decoded)
+        if(!decoded)
+            return null
+        
+        return decoded.userId
+        
+    }catch(e){
         return null
+    }
     
-    return decoded.userId
 }
 
 
@@ -44,13 +49,42 @@ wss.on('connection',(ws,request)=>{
     }
     
     users.push({
-        ws:ws,
+        ws,
         rooms:[],
-        userId:userId
+        userId
 
     })
     
     ws.on('message',(message)=>{
-        ws.send(message)
+        
+        const parsedData = JSON.parse(message as unknown as string)
+        if(parsedData.type === 'join_room'){
+            const user = users.find(x=>x.ws === ws) //checks if user is authenticatedly connected to the ws
+            user?.rooms.push(parsedData.roomId)
+        }
+
+        if(parsedData.type === 'leave_room'){
+            const user = users.find(x => x.ws === ws)
+            if(!user)
+                return
+
+            user.rooms = user?.rooms.filter(x=>x!==parsedData.roomId)
+        }
+
+        if(parsedData.type === "chat"){
+            const roomId = parsedData.roomId
+            const msg = parsedData.message
+
+            users.forEach(user => {
+                if(user.rooms.includes(roomId)){
+                    user.ws.send(JSON.stringify({
+                        type:"chat",
+                        message:msg,
+                        roomId
+                    }))
+                }
+            })
+        }
+        
     })
 })
